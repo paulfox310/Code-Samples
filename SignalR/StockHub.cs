235d2303d1b83c32557114a5c22bc61e1bbb7d61
Paul Fox <paul.fox310@gmail.com>
2019-05-30 14:19:54 -0700

    Add files via upload
    Some simple SignalR work that I did for a stock ticking application

---
+++ SignalR/StockHub.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+using BigMoney.Services;
+using BigMoney.Models.Domain;
+
+namespace Hubs
+{
+    public class StockHub : Hub
+    {
+        private IStocksService _stocks;
+
+        public StockHub(IStocksService stocks)
+        {
+            _stocks = stocks;
+        }
+
+        private static Timer aTimer;
+
+        public Task Stocks()
+        {
+            GroupStock stocks = _crawling.Stocks();
+			aTimer.Start();
+            return Clients.All.SendAsync("ReceiveStocks", stocks);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+            StartTimer();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+            CloseStocks();
+        }
+
+        public void CloseStocks()
+        {
+            aTimer.Stop();
+            aTimer.Dispose();
+        }
+
+        public void StartTimer()
+        {
+            aTimer = new Timer(30000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        public void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+			aTimer.Stop();
+            Stocks();
+        }
+    }
+}
